36b65bd1c1c57bab4afde07e5388a9276d377966
Williambellity <william.bellity1@gmail.com>
2021-04-15 10:42:22 +0200

    acces deleted

--- apps/acces/front/main.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Wity CMS
- * Système de gestion de contenu pour tous. * * @author Fofif * @version	$Id: apps/acces/front/main.php 0000 18-06-2011 Fofif $ */class AccesController extends WController {	public function launch() {
-		$this->view->assign('css', '/apps/acces/front/css/style.css');		$this->view->assign('pageTitle', "Informations pratiques pour accéder au Forum Est-Horizon");		$this->render('acces');	}}?>
