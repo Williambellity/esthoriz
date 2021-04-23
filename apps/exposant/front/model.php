547ca7d263716622c45e37391012fff14754c55d
Williambellity <william.bellity1@gmail.com>
2021-04-23 15:36:17 +0200

    exposant clean html php

--- apps/exposant/front/model.php
+++ apps/exposant/front/model.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Wity CMS
- * Système de gestion de contenu pour tous.
+ * Systï¿½me de gestion de contenu pour tous.
  *
  * @author Fofif <Johan Dufau>
  * @version	$Id: apps/brochure/front/model.php 0000 28-04-2011 Fofif $
@@ -18,16 +18,7 @@ class evenementModel {
 		if ($year == 0) {
 			$year = date('Y',time());
 		}
-		/*$prep = $this->db->prepare('
-			SELECT `entreprises`.id AS id, `entreprises`.name, `entreprises`.cat, `brochure`.website AS website2 FROM `entreprises`,`brochure` 
-			WHERE ( `entreprises`.id=`brochure`.firmid AND `phoning_step` =6 AND entreprises.id!=1254 AND SUBSTRING(name,1,1)!="-")ORDER BY name ASC 
- 		');*/
-		
-        /*$prep = $this->db->prepare('
-			SELECT `entreprises`.id AS id, `entreprises`.name, `entreprises`.cat, `brochure`.website AS website2 FROM `entreprises`,`brochure` 
-			WHERE ( `entreprises`.id=`brochure`.firmid AND entreprises.id!=1254 AND SUBSTRING(name,1,1)!="-")ORDER BY name ASC 
- 		');*/
-        
+	
         $prep = $this->db->prepare('
 			SELECT id FROM `entreprises` WHERE id!=1254 AND SUBSTRING(name,1,1)!="-"
  		');
